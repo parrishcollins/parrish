78b6ef162931788d8750ea8634dbe9fd186bc122
Parrish Collins <parrishcollins@gmail.com>
2016-01-13 12:00:31 -0700

    add gitingore to parrish

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<html>test</html>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset= UTF-8>
+		<title>About Parrish</title>
+	</head>
+		<body>
+			<h1>About Parrish</h1>
+			<p>a little bit of info on parrish</p>
+			<h2>Occupation</h2>
+				<p>Attorney but not the soul-sucking type alluded to by Dylan</p>
+			<h2>Goals</h2>
+				<p>retire early</p>
+				<p>learn to play the spoons</p>
+			<h2>Coding Knowledge</h2>
+				<p>more than other mammals with the exception of cats</p>
+		</body>
+</html>
